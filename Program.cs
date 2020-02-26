c8f9d7c3989aad85a7debb454206b5365a77a974
Ais9701 <aysielu2016@gmail.com>
2020-02-26 11:55:58 +0500

    Изменение подписи

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace LiAis3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("LiAis");
         }
     }
 }
